e6dff603907076f566ce63c878eab577cb1fefa0
Juan Pablo Vargas 'MrPakito <mrpakito@icloud.com>
2019-10-21 18:47:58 -0500

    Actualizacion backend-server

--- routes/usuario.js
+++ routes/usuario.js
@@ -43,7 +43,7 @@ app.get('/', (req, res, next) => {
 // Crear un nuevo usuario.
 // ======================================================
 
-app.post('/', mdAutenticacion.verificaToken, (req, res) => {
+app.post('/', (req, res) => {
     var body = req.body;
     var usuario = new Usuario({
         nombre: body.nombre,
